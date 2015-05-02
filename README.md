42ad393886492324eab44bd34b05c07e4ced237e
Giulio Cesare Solaroli <giulio.cesare@solaroli.it>
2015-05-02 16:07:32 +0200

    Fixed moving of taken pictures to USB key

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 tool to take and process pictures with a DSLR camera, using a Raspberry PI.
 
 This tools has been created to support the creation of a timelapse video that should last more than 16 months.
+
+
+## usbreset.c
+
+This module was copied from the (dwiel/gphoto2-timelapse)[https://github.com/dwiel/gphoto2-timelapse/blob/master/usbreset.c] project 
+
+To compile it, just run
+
+'''
+	cc usbreset.c -o usbreset
+'''
--- cheese.py
+++ cheese.py
@@ -3,33 +3,24 @@
 
 from datetime import datetime, timedelta
 import subprocess
-#from subprocess import Popen, PIPE
-
-import os
-#import piggyphoto
-
+import os, shutil
 
 CAMERA_TYPE = 'Canon'
+LOCAL_FOLDER = '/home/pi/cheese'
+PHOTO_FOLDER = '/mnt/photo'
+PHOTO_EXTENSIONS = ['cr2', 'jpg']
 
 def log (message):
 	print datetime.utcnow(), message
 
+
 def resetUsb (cameraType):
-	import os
-
-	log('reset usb')
-
 	process = subprocess.Popen(["lsusb"], stdout=subprocess.PIPE)
 	(output, err) = process.communicate()
 	exit_code = process.wait()
 
 	for line in output.split('\n') :
-#		log('line: %s' % line)
-
 		if cameraType in line:
-#			log('- line: %s' % line)
-#			log('- line fragments: %s - %s' % (line[4:7], line[15:18]))
-
 			process = subprocess.Popen(["./usbreset", "/dev/bus/usb/%s/%s" % (line[4:7], line[15:18])], stdout=subprocess.PIPE)
 			(output, err) = process.communicate()
 			exit_code = process.wait()
@@ -42,40 +33,35 @@ def turnOnCamera ():
 	pass
 
 
-def takePhoto ():
-	now = datetime.now()
-	dateformat = now.strftime('%Y-%m-%d_%H%M%S')
-	filename = "%s.%%C" % dateformat
-	
-	log("date format: %s" % dateformat)
-	log("file name: %s" % filename)
-#	"--set-config", "capturetarget=0", 
-	command = ["gphoto2", "--force-overwrite", "--capture-image-and-download", "--filename=%s" % filename]
-	
+def takePhoto (filename):
+	command = ["gphoto2", "--force-overwrite", "--capture-image-and-download", "--filename=%s.%%C" % filename]
 	result = subprocess.check_output(command)
-#	process = subprocess.Popen(command, stdout=subprocess.PIPE)
-#	(output, err) = process.communicate()
-#	exit_code = process.wait()
-	log(result)
-	
-
-# context = gp.Context()
-# camera = gp.Camera()
-# camera.init(context)
-# text = camera.get_summary(context)
-# print('Summary')
-# print('=======')
-# print(str(text))
-# camera.exit(context)
-#
-
-resetUsb(CAMERA_TYPE)
-turnOnCamera()
-takePhoto()
-resetUsb(CAMERA_TYPE)
 
 
-#camera = piggyphoto.camera()
-#print camera.abilities
-#C.capture_preview('preview.jpg')
-#C.capture_image('image.jpg')
+def movePictures (filename):
+	for extension in PHOTO_EXTENSIONS:
+		localFile = "%s/%s.%s" % (LOCAL_FOLDER, filename, extension)
+		targetFile = "%s/%s.%s" % (PHOTO_FOLDER, filename, extension)
+
+		try:
+			shutil.move(localFile, targetFile)
+		except:
+			if not os.path.isfile(targetFile):
+				raise Exception("something went wrong copying the file")
+		
+		os.remove(localFile)
+
+
+def cheese ():
+	now = datetime.now()
+	filename = now.strftime('%Y-%m-%d_%H%M%S')
+
+	resetUsb(CAMERA_TYPE)
+	turnOnCamera()
+	takePhoto(filename)
+	movePictures(filename)
+
+	resetUsb(CAMERA_TYPE)
+
+
+cheese ()
