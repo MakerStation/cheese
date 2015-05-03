7cfe1094dd92324fc452a047fd778c4a31cace60
Giulio Cesare Solaroli <giulio.cesare@solaroli.it>
2015-05-03 18:08:48 +0200

    Added script to start at boot

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ To compile it, just run
 '''
 	cc usbreset.c -o usbreset
 '''
+
+
+## rc.cheese
+Instructions to start the script on Raspberry boot also taken from the gphoto2-timelapse project
+
+There is also an rc script which you can use to start the timelapse script automatically whenever the computer (raspberry pi) is turned on.  To setup this feature run the following commands:
+
+sudo ln ./rc.cheese /etc/init.d/rc.cheese
+cd /etc/init.d/
+sudo update-rc.d rc.cheese defaults
--- cheese.py
+++ cheese.py
@@ -4,17 +4,26 @@
 from datetime import datetime, timedelta
 import subprocess
 import os, shutil
+import keen
+
 
 CAMERA_TYPE = 'Canon'
 LOCAL_FOLDER = '/home/pi/cheese'
-PHOTO_FOLDER = '/mnt/photo'
+PHOTO_FOLDER = '/media/photo'
 PHOTO_EXTENSIONS = ['cr2', 'jpg']
+S3_BUCKET = "timelapse"
+
+
+keen.project_id = ''
+keen.write_key  = ''
+
 
 def log (message):
 	print datetime.utcnow(), message
 
 
 def resetUsb (cameraType):
+	log(">>> reset USB")
 	process = subprocess.Popen(["lsusb"], stdout=subprocess.PIPE)
 	(output, err) = process.communicate()
 	exit_code = process.wait()
@@ -27,41 +36,169 @@ def resetUsb (cameraType):
 
 			if exit_code != 0:
 				print err
+	log("<<< reset USB")
 
 
-def turnOnCamera ():
+def turnCameraOn ():
 	pass
 
 
 def takePhoto (filename):
+	log(">>> take photo")
 	command = ["gphoto2", "--force-overwrite", "--capture-image-and-download", "--filename=%s.%%C" % filename]
 	result = subprocess.check_output(command)
+	log("<<< take photo")
 
 
 def movePictures (filename):
+	import traceback
+
+	log(">>> move pictures")
 	for extension in PHOTO_EXTENSIONS:
+		log("-- extension: %s" % extension)
 		localFile = "%s/%s.%s" % (LOCAL_FOLDER, filename, extension)
+		log("-- local file: %s" % localFile)
 		targetFile = "%s/%s.%s" % (PHOTO_FOLDER, filename, extension)
+		log("-- target file: %s" % targetFile)
 
 		try:
-			shutil.move(localFile, targetFile)
-		except:
-			if not os.path.isfile(targetFile):
-				raise Exception("something went wrong copying the file")
-		
-		os.remove(localFile)
+			log("-> copying ...")
+			shutil.copy(localFile, targetFile)
+			log("<- copying - DONE")
+		except Exception as e:
+			log("exception: %s", str(e))
+			pass
+
+		if os.path.isfile(targetFile):
+			log("-> removing local file ...")
+			os.remove(localFile)
+			log("<- removing local file - DONE")
+		else:
+			raise Exception("something went wrong copying the file")
+	log("<<< move pictures")
 
 
-def cheese ():
+def sendThumbnailToS3 (filename):
+	from boto.s3.connection import S3Connection
+	from boto.s3.key import Key
+	
+	file = "%s/%s.%s" % (LOCAL_FOLDER, filename, "jpg")
+	
+	connection = S3Connection()
+	bucket = connection.get_bucket(S3_BUCKET)
+	snapshot = Key(bucket)
+	snapshot.key = "%s.jpg" % filename
+	snapshot.set_contents_from_filename(file)
+
+
+def sendThumbnailViaHttpPost (filename):
+	pass
+
+
+def sendThumbnail (filename):
+	log(">>> send thumbnail")
+	sendThumbnailViaHttpPost(filename)
+	log("<<< send thumbnail")
+
+
+def connectGPRS ():
+	log(">>> connect GPRS")
+	process = subprocess.Popen(["sudo", "/home/pi/sakis3g/sakis3g", "connect", "USBMODEM=\"12d1:1001\"", "APN=\"ibox.tim.it\""], stdout=subprocess.PIPE)
+	(output, err) = process.communicate()
+	exit_code = process.wait()
+
+	if exit_code != 0:
+		raise Exception("something went wrong setting up GPRS connection")
+	log("<<< connect GPRS")
+
+
+def disconnectGPRS ():
+	log(">>> disconnect GPRS")
+	process = subprocess.Popen(["sudo", "/home/pi/sakis3g/sakis3g", "disconnect"], stdout=subprocess.PIPE)
+	(output, err) = process.communicate()
+	exit_code = process.wait()
+
+	if exit_code != 0:
+		raise Exception("something went wrong closing the GPRS connection")
+	log("<<< disconnect GPRS")
+
+
+def readLineFromSerialPort ():
+	#	http://www.texnological.blogspot.it/2014/03/raspberry-pi-read-data-from-usb-serial.html
+	return "Ore 14:55:23 Volts 13.50 Temp. 22.00"
+
+
+def collectStats (filename):
+	log(">>> collect stats")
+	statInfo = readLineFromSerialPort()
+
+	temperature = 10.5
+	batteryLevel = 95
+	dateTime = "2015-05-03_144844"
+	
+	rawSize = 20449214
+	jpgSize = 3629065
+	
+	stats = {
+		"environment": [ {
+			"temperature": temperature,
+			"batteryLevel": batteryLevel,
+			"dateTime": dateTime
+		} ],
+		"image": [ {
+			"name":    filename,
+			"rawSize": rawSize,
+			"jpgSize": jpgSize
+		} ]
+	}
+	log("<<< collect stats")
+	
+	return stats
+
+
+def sendStats (stats):
+#	https://github.com/keenlabs/KeenClient-Python
+
+#	from keen.client import KeenClient
+#	client = KeenClient(project_id="xxxx", write_key="yyyy")
+#	keen.add_event("sign_ups", {"username": "lloyd", "referred_by": "harry"})
+	log(">>> send stats")
+	keen.add_events(stats)
+	log("<<< send stats")
+
+
+def signalShuttingDown ():
+	log(">>> signal shutting down")
+	pass
+	log("<<< signal shutting down")
+
+
+def shutdown ():
+	log(">>> shutting down")
+	os.system('sudo shutdown now -h')
+	log("<<< shutting down")
+
+
+def cheese (filename):
+#	turnCameraOn()
+	takePhoto(filename)
+	movePictures(filename)
+	connectGPRS()
+	sendThumbnail(filename)
+	sendStats(collectStats(filename))
+	disconnectGPRS()
+	signalShuttingDown()
+	shutdown()
+
+
+def main ():
 	now = datetime.now()
 	filename = now.strftime('%Y-%m-%d_%H%M%S')
 
 	resetUsb(CAMERA_TYPE)
-	turnOnCamera()
-	takePhoto(filename)
-	movePictures(filename)
-
+	cheese (filename)
 	resetUsb(CAMERA_TYPE)
 
 
-cheese ()
+if __name__ == "__main__":
+	main()
